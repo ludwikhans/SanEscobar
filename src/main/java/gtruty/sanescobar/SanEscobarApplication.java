9aadc1019007f01ca65a264c1e753c5c39dcabe8
Grzegorz <grzegorztruty@gmail.com>
2022-06-27 21:32:19 +0200

    Initial commit

---
+++ src/main/java/gtruty/sanescobar/SanEscobarApplication.java
@@ -0,0 +1,13 @@
+package gtruty.sanescobar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanEscobarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanEscobarApplication.class, args);
+    }
+
+}
