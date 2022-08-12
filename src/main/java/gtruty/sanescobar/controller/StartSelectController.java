71b43d7fd24f304220fb52be97ab2904a4dabe60
Grzegorz <grzegorztruty@gmail.com>
2022-08-12 14:40:46 +0200

    Alfa 1.0

--- src/main/java/gtruty/sanescobar/controller/StartSelectController.java
+++ src/main/java/gtruty/sanescobar/controller/StartSelectController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.validation.Valid;
 
 @Controller
 public class StartSelectController {
--- src/main/java/gtruty/sanescobar/entities/GameEventEntity.java
+++ src/main/java/gtruty/sanescobar/entities/GameEventEntity.java
@@ -24,24 +24,17 @@ public class GameEventEntity {
 
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GameEventEntity that = (GameEventEntity) o;
-        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(info, that.info);
+    public GameEventEntity(int id, String name, String info) {
+        this.id = id;
+        this.name = name;
+        this.info = info;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, info);
-    }
-
-    public Integer getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(int id) {
         this.id = id;
     }
 
